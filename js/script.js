9aa419a23ee98eebb9de8b9a18f0983015cdf142
sonyanov <60970319+sonyanov@users.noreply.github.com>
2020-12-11 15:45:01 +0300

    lab8

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function openid(id){
+	var openid = document.getElementById(id)
+	openid.style.display = "block"
+}
+
+function closeid(id){
+	var closeid = document.getElementById(id)
+	closeid.style.display = "none"
+}
+
+function parse_catalog(path){
+
+var tmpl = document.getElementById("catalog_tmpl").content.children[0];
+var catalog = document.getElementById("grid-catalog")
+
+$.getJSON(path)
+	.done(function(data){
+		data["products"].forEach(element => {
+			var catalog_card = tmpl.cloneNode(true);
+			catalog_card.children[0].src = element["img"]
+			catalog_card.children[1].textContent = element["title"]
+			catalog_card.children[2].textContent = element["name"]
+			catalog_card.children[3].textContent = element["price"]
+
+			catalog.appendChild(catalog_card)
+		});
+	});
+}
+
+parse_catalog("catalog.json")
